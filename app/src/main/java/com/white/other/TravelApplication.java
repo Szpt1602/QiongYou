f1082f155f96f82fc99f2260699a38d04965392e
A8 <375514627@qq.com>
2016-06-14 09:09:40 +0800

    white

--- app/src/main/java/com/white/other/TravelApplication.java
+++ app/src/main/java/com/white/other/TravelApplication.java
@@ -2,6 +2,9 @@ package com.white.other;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 /**
  * Created by A8 on 2016/6/2.
  */
@@ -11,7 +14,11 @@ public class TravelApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-
+        //获取ImageLoader默认的设置
+        ImageLoaderConfiguration configuration = ImageLoaderConfiguration
+                .createDefault(this);
+        //ImageLoader初始化操作
+        ImageLoader.getInstance().init(configuration);
 
     }
 }
